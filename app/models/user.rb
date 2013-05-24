6cbe95db5398c3797a9a70540ccaeaa5c77c53ca
Michael Hettegger <mhettegger@fh-salzburg.ac.at>
2013-05-24 12:11:30 +0200

    Fixed plaintext password security issue. Included gem bcrypt for encrypting. Added column password_digest to table user. used has_secure_password module. Replaced authenticate method in session_controller. Wrote a new test and fixed one failing test.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :articles
 
   attr_accessible :name, :email, :password, :homepage, :is_admin
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,7 @@ describe User do
 
   it "can authenticate with right mail + password" do
     u1 = User.create!( :name => "dummy", :email => "foo@bar.com", :password => "geheim" )
-    u2 = User.authenticate( "foo@bar.com", "geheim" )
+    u2 = u1.authenticate("geheim" )
     u2.should_not be_nil
     u1.id.should eql(u2.id)
   end
@@ -20,6 +20,15 @@ describe User do
     u1 = User.create!( :name => "dummy", :email => "foo@bar.com", :password => "geheim" )
     u2 = User.authenticate( "foo@bar.com", "falsch" )
     u2.should be_nil
+  end
+
+  it "encrypts the password" do
+    u1 = User.new( :name => "dummy", :email => "foo@bar.com", :password => "geheim" )
+    u1.save
+    u2 = User.find_by_email("foo@bar.com")
+    u2.password_digest.should_not be_nil
+    u2.password_digest.should_not eql(u1.password)
+    u2.password.should be_nil
   end
 
 end
